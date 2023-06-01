4853fcd54cf60461e8b1879d002d8fa45ff74479
Aaron Yang <zhenhuayang@outlook.com>
2023-06-01 11:10:50 -0500

    update

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,22 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBZZBAf49ZlGA23L-Bvy8WjCzNM6KMZjkM",
+//     authDomain: "sodaa-db.firebaseapp.com",
+//     databaseURL: "https://sodaa-db-default-rtdb.firebaseio.com",
+//     projectId: "sodaa-db",
+//     storageBucket: "sodaa-db.appspot.com",
+//     messagingSenderId: "558604021861",
+//     appId: "1:558604021861:web:16f257bc0d1192f366968c",
+//     measurementId: "G-GVM2X55F29"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBZZBAf49ZlGA23L-Bvy8WjCzNM6KMZjkM",
+    apiKey: env.secrets.FIREBASE_API_KEY,
     authDomain: "sodaa-db.firebaseapp.com",
     databaseURL: "https://sodaa-db-default-rtdb.firebaseio.com",
     projectId: "sodaa-db",
     storageBucket: "sodaa-db.appspot.com",
     messagingSenderId: "558604021861",
-    appId: "1:558604021861:web:16f257bc0d1192f366968c",
+    appId: env.secrets.FIREBASE_APP_ID,
     measurementId: "G-GVM2X55F29"
 };
 
